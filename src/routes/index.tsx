f41c174d4d8fb32befd599ec2feed368791b2663
gustavocadev <gustavocadev@gmail.com>
2024-05-31 11:01:32 -0500

    Add login ui

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,16 +1,21 @@
 import { component$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
+import { Input } from "~/components/ui/input/input";
 
 export default component$(() => {
   return (
-    <>
-      <h1>Hi 👋</h1>
-      <div>
-        Can't wait to see what you build with qwik!
-        <br />
-        Happy coding.
+    <main class="mx-auto max-w-5xl space-y-4 py-12 ">
+      <h2 class="text-4xl">Log in to have fun and learn faster</h2>
+
+      <div class="flex flex-col gap-2">
+        <Input type="email" name="" placeholder="Email" required />
+
+        <Input type="password" placeholder="Password" required />
+        <button class="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700">
+          Log in
+        </button>
       </div>
-    </>
+    </main>
   );
 });
 
