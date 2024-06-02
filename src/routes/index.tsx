c0539e2ab2e33b2c33c8210855575634211bf442
gustavocadev <gustavocadev@gmail.com>
2024-06-02 13:40:54 -0500

    chore: Update login page content

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,20 +1,12 @@
 import { component$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
-import { Input } from "~/components/ui/input/input";
 
+// Welcome to the user to login the app
 export default component$(() => {
   return (
-    <main class="mx-auto max-w-5xl space-y-4 py-12 ">
-      <h2 class="text-4xl">Log in to have fun and learn faster</h2>
-
-      <div class="flex flex-col gap-2">
-        <Input type="email" name="" placeholder="Email" required />
-
-        <Input type="password" placeholder="Password" required />
-        <button class="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700">
-          Log in
-        </button>
-      </div>
+    <main>
+      <h2>Hi </h2>
+      <p>Welcome to the Qwik app</p>
     </main>
   );
 });
