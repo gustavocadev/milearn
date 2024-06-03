88f88d5298bffe5ce056122abf7a3eb832f9d565
gustavocadev <gustavocadev@gmail.com>
2024-06-03 13:07:03 -0500

    chore: Update authed index page

--- src/routes/(authed)/index.tsx
+++ src/routes/(authed)/index.tsx
@@ -1,13 +1,16 @@
 import { component$ } from "@builder.io/qwik";
-import type { DocumentHead } from "@builder.io/qwik-city";
+import { type DocumentHead } from "@builder.io/qwik-city";
 
 // Welcome to the user to login the app
 export default component$(() => {
   return (
-    <main>
-      <h2>Hi </h2>
-      <p>Welcome to the Qwik app</p>
-    </main>
+    <>
+      <h2 class="text-4xl">
+        Hi, you're
+        <span class="text-blue-500"> logged in!</span>
+      </h2>
+      <p>Enjoy your stay and feel free to explore the app. If you have any</p>
+    </>
   );
 });
 
