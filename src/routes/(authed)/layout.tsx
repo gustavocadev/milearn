281d38f808061c6151bbe6534ac1c362280d907d
gustavocadev <gustavocadev@gmail.com>
2024-06-03 12:57:37 -0500

    chore: Update layout.tsx to add authentication check in onRequest handler

--- src/routes/(authed)/layout.tsx
+++ src/routes/(authed)/layout.tsx
@@ -1,5 +1,15 @@
 import { Slot, component$ } from "@builder.io/qwik";
+import type { RequestHandler } from "@builder.io/qwik-city";
 import { Sidebar } from "~/components/shared/sidebar";
+import { handleRequest } from "~/server/services/auth/lucia";
+
+export const onRequest: RequestHandler = async (event) => {
+  const authRequest = handleRequest(event);
+  const { user } = await authRequest.validateUser();
+  if (!user) {
+    throw event.redirect(303, "/login");
+  }
+};
 
 export default component$(() => {
   return (
