e6f5a2c8105c7ea4b04e439b06ef81fb99a65088
gustavocadev <gustavocadev@gmail.com>
2024-06-03 12:52:09 -0500

    chore: Add sidebar component

---
+++ src/components/shared/sidebar.tsx
@@ -0,0 +1,44 @@
+import { component$ } from "@builder.io/qwik";
+import { Link } from "@builder.io/qwik-city";
+
+export const Sidebar = component$(() => {
+  const userRoutes = [
+    {
+      name: "Home",
+      path: "/",
+    },
+    {
+      name: "Videos",
+      path: "/videos",
+    },
+    {
+      name: "Conversations",
+      path: "/conversations",
+    },
+  ];
+
+  return (
+    <aside
+      class="h-screen w-64 -translate-x-full border-r border-gray-200 bg-white transition-transform sm:translate-x-0"
+      aria-label="Sidebar"
+    >
+      <div class="h-full overflow-y-auto bg-white px-3 py-4">
+        <h2 class="p-2 text-2xl font-semibold">LOGO</h2>
+        <ul class="space-y-2 font-medium">
+          {userRoutes.map((route) => {
+            return (
+              <li key={route.path}>
+                <Link
+                  href={route.path}
+                  class="group flex items-center rounded-sm p-2 hover:bg-gray-300"
+                >
+                  <span class="ms-3">{route.name}</span>
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </aside>
+  );
+});
