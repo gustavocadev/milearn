efc633806ebefd04fc12f607c6ef6b0f93ee31ff
gustavocadev <gustavocadev@gmail.com>
2024-06-02 20:12:06 -0500

    chore: Update login page with improved form handling and authentication logic

--- src/routes/login/index.tsx
+++ src/routes/login/index.tsx
@@ -1,18 +1,82 @@
 import { component$ } from "@builder.io/qwik";
-import { Link } from "@builder.io/qwik-city";
+import { Form, Link, routeAction$, z, zod$ } from "@builder.io/qwik-city";
 import { Button } from "~/components/ui/button/button";
 import { Input } from "~/components/ui/input/input";
+import { handleRequest, lucia } from "~/server/auth/lucia";
+import { db } from "~/server/drizzle/db";
+import { userTable } from "../../../drizzle/schema";
+import { eq } from "drizzle-orm";
+import { verifyPassword } from "qwik-lucia";
+
+export const useLoginAction = routeAction$(
+  async (values, { cookie, fail, redirect }) => {
+    // Important! Use `handleRequest` to handle the authentication request
+    const authRequest = handleRequest({ cookie });
+
+    try {
+      //1. search for user
+      const [user] = await db
+        .select({
+          id: userTable.id,
+          passwordHash: userTable.passwordHash,
+          username: userTable.username,
+        })
+        .from(userTable)
+        .where(eq(userTable.username, values.username));
+
+      //2. if user is not found, throw error
+      if (!user) {
+        return fail(400, {
+          message: "Incorrect username or password",
+        });
+      }
+
+      // 3. validate password
+      const isValidPassword = await verifyPassword(
+        user.passwordHash,
+        values.password,
+      );
+
+      if (!isValidPassword) {
+        return fail(400, {
+          message: "Incorrect username or password",
+        });
+      }
+
+      // 4. create session
+      const session = await lucia.createSession(user.id, {});
+
+      authRequest.setSession(session); // set session cookie
+      console.log("session", session);
+    } catch (e) {
+      console.error(e);
+    }
+    // make sure you don't throw inside a try/catch block!
+    throw redirect(303, "/");
+  },
+  // validate the input
+  zod$({
+    username: z.string(),
+    password: z.string(),
+  }),
+);
 
 export default component$(() => {
+  const loginAction = useLoginAction();
   return (
     <main class="mx-auto max-w-5xl space-y-4 py-12 ">
       <h2 class="text-4xl">Log in to have fun and learn faster</h2>
 
-      <div class="flex flex-col gap-2">
-        <Input type="text" name="" placeholder="Username" required />
+      <Form class="flex flex-col gap-2" action={loginAction}>
+        <Input type="text" name="username" placeholder="Username" required />
 
-        <Input type="password" placeholder="Password" required />
-        <Button>Log in</Button>
+        <Input
+          type="password"
+          name="password"
+          placeholder="Password"
+          required
+        />
+        <Button type="submit">Log in</Button>
 
         <p>
           Don't have an account?{" "}
@@ -20,7 +84,11 @@ export default component$(() => {
             Sign up
           </Link>
         </p>
-      </div>
+
+        {loginAction.value?.message && (
+          <p class="font-bold text-red-500">{loginAction.value.message}</p>
+        )}
+      </Form>
     </main>
   );
 });
