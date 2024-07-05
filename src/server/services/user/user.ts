dfc0b0facfd438d37c1f8b05b751255836382320
gustavocadev <gustavocadev@gmail.com>
2024-07-04 19:25:58 -0500

    Add get user by id function

--- src/server/services/user/user.ts
+++ src/server/services/user/user.ts
@@ -1,4 +1,11 @@
-export const getUserById = (id: string) => { };
+import { db } from "~/server/drizzle/db";
+import { SelectUser, userTable } from "../../../../drizzle/schema";
+import { eq } from "drizzle-orm";
+
+export const getUserById = async (id: string): Promise<SelectUser> => { 
+  const [user] = await db.select().from(userTable).where(eq(userTable.id, id))
+  return user
+};
 
 export const deleteUserById = (id: string) => { }
 
