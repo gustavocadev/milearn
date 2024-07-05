dd238911f06fccd36772a481d90bb81c8a68e504
gustavocadev <gustavocadev@gmail.com>
2024-07-04 19:26:40 -0500

    chore: Refactor layout component and add language and conversation providers

--- src/routes/(authed)/layout.tsx
+++ src/routes/(authed)/layout.tsx
@@ -1,14 +1,10 @@
-// @ts-nocheck
-
 import { Slot, component$ } from "@builder.io/qwik";
-import {
-  useLocation,
-  useNavigate,
-  type RequestHandler,
-} from "@builder.io/qwik-city";
-import { LuCheck } from "@qwikest/icons/lucide";
+import { type RequestHandler } from "@builder.io/qwik-city";
+import { NavbarAuth } from "~/components/shared/navbar-auth";
 import { Sidebar } from "~/components/shared/sidebar";
-import { Select } from "~/components/ui/select/select";
+import { ConversationProvider } from "~/context/conversation/ConversationProvider";
+import { LanguageProvider } from "~/context/language/LanguageProvider";
+
 import { handleRequest } from "~/server/services/auth/lucia";
 
 export const onRequest: RequestHandler = async (event) => {
@@ -20,63 +16,21 @@ export const onRequest: RequestHandler = async (event) => {
 };
 
 export default component$(() => {
-  const languages = [
-    "English",
-    "Spanish",
-    "French",
-    "German",
-    "Italian",
-    "Portuguese",
-    "Russian",
-    "Chinese",
-    "Japanese",
-    "Korean",
-    "Arabic",
-    "Hindi",
-    "Bengali",
-    "Punjabi",
-    "Urdu",
-    "Turkish",
-  ];
-  const nav = useNavigate();
-  const loc = useLocation();
-
   return (
-    <div class="flex">
-      <div class="xl:w-2/12">
-        <Sidebar />
-      </div>
-      <div class="w-full">
-        <nav class="flex items-center p-4">
-          <Select.Root
-            onChange$={(value: string) => {
-              const url = new URL(loc.url);
-              url.searchParams.set("lang", value.toLowerCase());
-              nav(url.href);
-            }}
-          >
-            {/* <Select.Label>Select a language</Select.Label> */}
-            <Select.Trigger>
-              <Select.DisplayValue placeholder="Select an option" />
-            </Select.Trigger>
-            <Select.Popover gutter={8}>
-              <Select.Listbox>
-                {languages.map((user) => (
-                  <Select.Item key={user}>
-                    <Select.ItemLabel>{user}</Select.ItemLabel>
-                    <Select.ItemIndicator>
-                      <LuCheck class="h-4 w-4" />
-                    </Select.ItemIndicator>
-                  </Select.Item>
-                ))}
-              </Select.Listbox>
-            </Select.Popover>
-          </Select.Root>
-        </nav>
-        <main class="px-12">
-          <Slot />
-        </main>
-      </div>
-    </div>
+    <LanguageProvider>
+      <ConversationProvider>
+        <div class="flex">
+          <div class="xl:w-2/12">
+            <Sidebar />
+          </div>
+          <div class="w-full">
+            <NavbarAuth />
+            <main class="px-12">
+              <Slot />
+            </main>
+          </div>
+        </div>
+      </ConversationProvider>
+    </LanguageProvider>
   );
 });
